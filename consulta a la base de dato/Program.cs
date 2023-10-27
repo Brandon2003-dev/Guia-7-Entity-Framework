9e8c35544fc2c40d30559b2381884d408f229ceb
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-10-26 20:32:44 -0600

    Update Program.cs

--- consulta a la base de dato/Program.cs
+++ consulta a la base de dato/Program.cs
@@ -1,79 +1,131 @@
-﻿using consulta_a_la_base_de_dato;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
-
+using Guia-7-Entity-Framework;
 
 class Program
 {
     static void Main()
     {
-
         while (true)
         {
             using (var context = new Context())
             {
-                Console.WriteLine("\nLista de Estudiantes:");
-
-                using (var contextdb = new Context())
+                Console.WriteLine("Lista de Estudiantes:");
+                foreach (var estudiante in context.Estudiante.ToList())
                 {
-                    contextdb.Database.EnsureCreated();
-
-                    var estudiante = new EstudianteUNAB() { Nombre = "Pepito", Apellido = "Pérez" };
-                    contextdb.Add(estudiante);
-                    contextdb.SaveChanges();
-
-                    foreach (var s in context.Estudiante.ToList())
-                    {
-                        Console.WriteLine($"ID: {s.Id}, Nombre: {s.Nombre} {s.Apellido}, Edad: {s.Edad}, Sexo: {s.Sexo}");
-                    }
-                }
-
-                Console.WriteLine("\nIngrese los datos de los nuevos estudiantes:");
-
-                Console.Write("Nombre: ");
-                var nombre = Console.ReadLine();
-
-                Console.Write("Apellido: ");
-                var apellido = Console.ReadLine();
-
-                Console.Write("Sexo: ");
-                var sexo = Console.ReadLine();
-
-                Console.Write("Edad: ");
-                if (int.TryParse(Console.ReadLine(), out int edad))
-                {
-                    var nuevoEstudiante = new EstudianteUNAB
-                    {
-                        Nombre = nombre,
-                        Apellido = apellido,
-                        Sexo = sexo,
-                        Edad = edad
-                    };
-
-                    try
-                    {
-                        context.Estudiante.Add(nuevoEstudiante);
-                        context.SaveChanges();
-                        Console.WriteLine("Estudiante agregado correctamente.");
-                    }
-                    catch (DbUpdateException ex)
-                    {
-                        Console.WriteLine("Error al agregar estudiante. Asegúrate de que los datos sean válidos.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Edad no válida. Intente nuevamente.");
-                }
-
-                Console.Write("¿Si desea agregar más estudiantes, presione (S) y si ya no desea ingresar, presione (N): ");
-                var respuesta = Console.ReadLine();
-                if (respuesta?.Trim().ToLower() != "s")
-                {
-                    break;
+                    Console.WriteLine($"ID: {estudiante.Id}, Nombre: {estudiante.Nombre}, Edad: {estudiante.Edad}, Sexo: {estudiante.Sexo}\n");
                 }
             }
-        }
-    }
+
+            using (var context = new Context())
+            {
+                Console.WriteLine("Ingresar el Id que desea modificar ");
+
+                var id = Console.ReadLine();
+                var persona = context.Estudiante.FirstOrDefault(p => p.Id == Int32.Parse(id));
+                if (persona != null)
+                {
+                    persona.Apellido = "Atributo modificado";
+                    context.SaveChanges();
+                    if (persona != null)
+                    {
+                        Console.WriteLine("¿Qué atributo desea modificar? (Nombre, Apellido, Sexo, Edad)");
+                        var eleccion = Console.ReadLine().ToLower().Trim();
+
+                        switch (eleccion)
+                        {
+                            case "nombre":
+                                Console.WriteLine("Ingresar el nuevo nombre:");
+                                persona.Nombre = Console.ReadLine();
+                                break;
+                            case "apellido":
+                                Console.WriteLine("Ingresar el nuevo apellido:");
+                                persona.Apellido = Console.ReadLine();
+                                break;
+                            case "sexo":
+                                Console.WriteLine("Ingresar el nuevo sexo:");
+                                persona.Sexo = Console.ReadLine();
+                                break;
+                            case "edad":
+                                Console.WriteLine("Ingresar la nueva edad:");
+                                persona.Edad = int.Parse(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("El atributo ingresado no es válido");
+                                return;
+                        }
+
+                        context.SaveChanges();
+                        Console.WriteLine("El registro se ha modificado correctamente");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El Id ingresado es inválido");
+                    }
+                }
+            }
+
+            using (var context = new Context())
+            {
+                Console.WriteLine("Ingresar el Id del registro que desea eliminar");
+                var Id = Console.ReadLine();
+                var persona = context.Estudiante.SingleOrDefault(p => p.Id == Int32.Parse(Id));
+                if (persona != null)
+                {
+                    context.Estudiante.Remove(persona);
+                    context.SaveChanges();
+                }
+
+
+                //Console.WriteLine("\nIngrese los datos de los nuevos estudiantes:");
+
+                //Console.Write("Nombre: ");
+                //var nombre = Console.ReadLine();
+
+                //Console.Write("Apellido: ");
+                //var apellido = Console.ReadLine();
+
+                //Console.Write("Sexo: ");
+                //var sexo = Console.ReadLine();
+
+                //Console.Write("Edad: ");
+                //if (int.TryParse(Console.ReadLine(), out int edad))
+                //{
+                //    var nuevoEstudiante = new EstudianteUNAB
+                //    {
+                //        Nombre = nombre,
+                //        Apellido = apellido,
+                //        Sexo = sexo,
+                //        Edad = edad
+                //    };
+
+                //    using (var context = new Context())
+                //    {
+                //        try
+                //        {
+                //            context.Estudiante.Add(nuevoEstudiante);
+                //            context.SaveChanges();
+                //            Console.WriteLine("Estudiante agregado correctamente.");
+                //        }
+                //        catch (DbUpdateException ex)
+                //        {
+                //            Console.WriteLine("Error al agregar estudiante. Asegúrate de que los datos sean válidos.");
+                //        }
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Edad no válida. Intente nuevamente.");
+                //}
+
+                //Console.Write("¿Si de sea agregar mas estudiante presione (S) y si ya no de sea ingresar presione (N): ");
+                //var respuesta = Console.ReadLine();
+                //if (respuesta?.Trim().ToLower() != "s")
+                //{
+                //    break;
+                //}
+            }
+        }
+    }
 }

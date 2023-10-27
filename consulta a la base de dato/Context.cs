e1e422845a4ad2360b061b3b57557a33cd9d617a
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-10-26 20:08:05 -0600

    Add files via upload

---
+++ consulta a la base de dato/Context.cs
@@ -0,0 +1,15 @@
+﻿using consulta_a_la_base_de_dato;
+using Microsoft.EntityFrameworkCore;
+
+public class Context : DbContext
+{
+
+    public DbSet<EstudianteUNAB> Estudiante{ get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-I6DQEF5\\SQLEXPRESS;Database=Program2;Trusted_Connection=True;");
+        }
+    }
+}
